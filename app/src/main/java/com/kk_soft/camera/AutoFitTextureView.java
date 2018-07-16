6eeebafb27001d561c48c0faf5832da79dd78d17
kikuda <kousaku.kikuda@worm.co.jp>
2018-07-16 20:20:28 +0900

    first commit

---
+++ app/src/main/java/com/kk_soft/camera/AutoFitTextureView.java
@@ -0,0 +1,48 @@
+package com.kk_soft.camera;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.TextureView;
+
+public class AutoFitTextureView extends TextureView {
+    private int ratioWidth = 0;
+    private int ratioHeight = 0;
+
+    public AutoFitTextureView(Context context) {
+        this(context, null);
+    }
+    public AutoFitTextureView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public AutoFitTextureView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setAspectRatio(int width, int height) {
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Size cannot be negative.");
+        }
+        ratioWidth = width;
+        ratioHeight = height;
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+/*
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        if (ratioWidth == 0 || ratioHeight == 0) {
+            setMeasuredDimension(width, height);
+        } else {
+            if (width < height * ratioWidth / ratioHeight) {
+                setMeasuredDimension(width, width * ratioHeight / ratioWidth);
+            } else {
+                setMeasuredDimension(height * ratioWidth / ratioHeight, height);
+            }
+        }
+        */
+    }
+}
